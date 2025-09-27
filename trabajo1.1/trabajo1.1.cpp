b0ed01c7d800b6e09a3b98287bdf44ec46ad79f4
ahernandezconstantini1-dot <ahernandezconstantini1@gmail.com>
2025-09-27 15:40:33 -0600

    cambios

--- trabajo1.1/trabajo1.1.cpp
+++ trabajo1.1/trabajo1.1.cpp
@@ -1,4 +1,30 @@
 #include <iostream>
+using namespace std;
+
+// Cambia valores del booleano
+void modificar(bool arr[], int n);
+
+// Cambia valores en arreglo de enteros y los imprime
+void modificar(int arr[], int n);
+
+int main() {
+
+    bool buleano[5];
+    modificar(buleano, 5);
+
+    cout << " Booleano \n";
+    for (int i = 0; i < 5; i++) {
+        cout << "Buleano[" << i << "] = "
+            << buleano[i] << endl;
+    }
+
+    // Arreglo de enteros
+    cout << " Enteros ";
+    int nums[10] = { 0,1,2,3,4,5,6,7,8,9 };
+    modificar(nums, 10);
+
+    return 0;
+}
 
 // Cambia valores del booleano
 void modificar(bool arr[], int n) {
@@ -12,26 +38,7 @@ void modificar(int arr[], int n) {
     int* puntero = arr;
     for (int i = 0; i < n; i++) {
         *puntero = *puntero % 2;
-        std::cout << "Direccion: " << puntero << "  Valor: " << *puntero << std::endl;
+        cout << "Direccion: " << puntero << "  Valor: " << *puntero << endl;
         puntero++;
     }
-}
-
-int main() {
-    // Arreglo de booleanos
-    bool buleano[5];
-    modificar(buleano, 5);
-
-    std::cout << " Booleano \n";
-    for (int i = 0; i < 5; i++) {
-        std::cout << "Buleano[" << i << "] = "
-            << std::boolalpha << buleano[i] << std::endl;
-    }
-
-    // Arreglo de enteros
-    std::cout << "\n Enteros \n";
-    int nums[10] = { 0,1,2,3,4,5,6,7,8,9 };
-    modificar(nums, 10);
-
-    return 0;
 }
